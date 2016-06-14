8b8b45598b537b6bc92a9096c7a01f843ad4bdcb
Lyle Denman <denmanlp@gmail.com>
2016-06-14 14:34:11 -0500

    Updates from PC -- anticipate Merge Conflict

--- src/graphDraw.js
+++ src/graphDraw.js
@@ -1,13 +1,20 @@
 /**
- * Created by wally on 6/13/16.
+ * Created by Lyle Denman on 6/13/16.
  */
 var GraphDraw = React.createClass({
+  getInitialState: function() {
+    return {name:"lyle"}
+  },
   render: function() {
-
+    return (
+        <svg>
+          <circle cx={300} cy={50} r={10} fill="red" />
+        </svg>
+    )
   }
 });
 
 ReactDOM.render(
-    <GraphDraw />,
-    document.getElementById('graph-draw')
+  <GraphDraw />,
+  document.getElementById('graph-draw')
 );
