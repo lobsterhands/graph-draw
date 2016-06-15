6e152b2e367f75f2ae475bb260bbf4ad2b72a32f
Lyle Denman <denmanlp@gmail.com>
2016-06-15 13:15:11 -0500

    Changes

--- src/js/module1.jsx
+++ src/js/module1.jsx
@@ -2,5 +2,5 @@
  * Created by Lyle on 6/15/2016.
  */
 console.log("module 1 stuff");
-console.log("module 1 extra stuff");
-console.log("module 1 extra stuffx2");
+console.log("module 1 extra a");
+console.log("module 1 extra stuffx8");
