e841dc338fdf1907d4f87c825827911bc0913d58
Lyle Denman <denmanlp@gmail.com>
2016-06-15 12:44:20 -0500

    Add node_modules

---
+++ js/module1.jsx
@@ -0,0 +1,4 @@
+/**
+ * Created by Lyle on 6/15/2016.
+ */
+console.log("module 1 stuff");
---
+++ js/scripts.jsx
@@ -0,0 +1,6 @@
+/**
+ * Created by Lyle on 6/15/2016.
+ */
+require('./module1.jsx');
+require('./module2.jsx');
+// Change these to jsx and see what happens
