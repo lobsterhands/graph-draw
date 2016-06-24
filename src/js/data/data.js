adf824fdcee8b233e859d90628714f7e97247045
Lyle Denman <denmanlp@gmail.com>
2016-06-23 20:10:30 -0500

    Start with empty array

--- src/js/data/data.js
+++ src/js/data/data.js
@@ -2,10 +2,10 @@
  * Created by wally on 6/17/16.
  */
 export default [ 
-    {x: 100, y: 100, radius: 20},
-    {x: 35, y: 200, radius: 20},
-    {x: 200, y: 200, radius: 20},
-    {x: 125, y: 270, radius: 20},
-    {x: 500, y: 350, radius: 20},
-    {x: 550, y: 320, radius: 20}
+    // {x: 100, y: 100, radius: 20},
+    // {x: 35, y: 200, radius: 20},
+    // {x: 200, y: 200, radius: 20},
+    // {x: 125, y: 270, radius: 20},
+    // {x: 500, y: 350, radius: 20},
+    // {x: 550, y: 320, radius: 20}
 ];
