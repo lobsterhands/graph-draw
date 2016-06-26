21124809730fa21246ae15f9d9d353980b13a093
Lyle Denman <denmanlp@gmail.com>
2016-06-26 00:09:06 -0500

    Begin with empty data

--- src/js/data/data.js
+++ src/js/data/data.js
@@ -5,7 +5,7 @@ export default [
     // {x: 100, y: 100, radius: 20},
     // {x: 35, y: 200, radius: 20},
     // {x: 200, y: 200, radius: 20},
-    // {x: 125, y: 270, radius: 20},
-    // {x: 500, y: 350, radius: 20},
-    // {x: 550, y: 320, radius: 20}
+    // {x: 500, y: 350, radius: 20, connections: []},
+    // {x: 550, y: 320, radius: 20, connections: []},
+    // {x: 125, y: 270, radius: 20, connections: []}
 ];
